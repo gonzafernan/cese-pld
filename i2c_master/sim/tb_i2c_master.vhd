7c832fbde71d52a3ac53f1cc875d36f93d130f50
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2023-10-02 20:02:40 -0300

    Added input save and modified testbench

--- i2c_master/sim/tb_i2c_master.vhd
+++ i2c_master/sim/tb_i2c_master.vhd
@@ -13,13 +13,20 @@ architecture tb_i2c_master_arch of tb_i2c_master is
         port(
             clock: in std_logic;
             reset: in std_logic;
+            read_write: in std_logic;
+            mosi_data: in std_logic_vector(7 downto 0);
+            register_address: in std_logic_vector(7 downto 0);
             slave_address: in std_logic_vector(6 downto 0)
         );
     end component;
 
     signal clock: std_logic;
     signal reset: std_logic;
+    signal read_write: std_logic;
+    signal mosi_data: std_logic_vector(7 downto 0);
+
     constant slave_address: std_logic_vector(6 downto 0) := std_logic_vector(to_unsigned(16#42#, 7));
+    constant register_address: std_logic_vector(7 downto 0) := "01010101";
 
 begin
 
@@ -27,6 +34,9 @@ begin
         port map(
             clock => clock,
             reset => reset,
+            read_write => read_write,
+            mosi_data => mosi_data,
+            register_address => register_address,
             slave_address => slave_address
         );
 
@@ -35,9 +45,19 @@ begin
     begin
         clock <= '0';
         reset <= '0';
+        read_write <= '0';
+        mosi_data <= "00000011";
         wait for 50 ps;
+        reset <= '1';
         while (i < 255) loop
-            reset <= '1';
+            clock <= not clock;
+            wait for 10 ps;
+            i := i + 2;
+        end loop;
+        read_write <= '1';
+        mosi_data <= "00110000";
+        i := 0;
+        while (i < 255) loop
             clock <= not clock;
             wait for 10 ps;
             i := i + 2;
--- i2c_master/src/i2c_master.vhd
+++ i2c_master/src/i2c_master.vhd
@@ -6,8 +6,8 @@ use IEEE.numeric_std.all;
 
 entity i2c_master is
     generic(
-        -- DATA_WIDTH: integer := 8;
-        -- REGISTER_WIDTH: integer := 8;
+        DATA_WIDTH: integer := 8;
+        REGISTER_WIDTH: integer := 8;
         ADDRESS_WIDTH: integer := 7
     );
     port(
@@ -18,11 +18,11 @@ entity i2c_master is
         -- I2C master module enable
         -- enable: in std_logic;
         -- (0) write, (1) read
-        -- read_write: in std_logic;
+        read_write: in std_logic;
         -- Data to be written to a register during an I2C write operation
-        -- mosi_data: in std_logic_vector(DATA_WIDTH-1 downto 0);
+        mosi_data: in std_logic_vector(DATA_WIDTH-1 downto 0);
         -- Register where data is read/written
-        -- register_address: in std_logic_vector(REGISTER_WIDTH-1 downto 0);
+        register_address: in std_logic_vector(REGISTER_WIDTH-1 downto 0);
         -- Slave I2C address
         slave_address: in std_logic_vector(ADDRESS_WIDTH-1 downto 0)
         -- Data to be read during a I2C read operation
@@ -35,6 +35,9 @@ architecture i2c_master_arq of i2c_master is
     constant IDLE: std_logic_vector(1 downto 0) := "00";
     signal state: std_logic_vector(1 downto 0); -- FSM state
     signal saved_slave_address: std_logic_vector(ADDRESS_WIDTH downto 0);
+    signal saved_register_address: std_logic_vector(REGISTER_WIDTH-1 downto 0);
+    signal saved_mosi_data: std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal saved_read_write: std_logic;
 
 begin
 
@@ -44,11 +47,13 @@ begin
             if reset = '0' then
                 -- Reset action
                 state <= IDLE;
-                saved_slave_address <= (ADDRESS_WIDTH downto 0 => '0');
             else
                 case state is
                     when IDLE =>
-                        saved_slave_address <= slave_address & "0";
+                        saved_slave_address <= slave_address & "0"; -- Save slave address
+                        saved_register_address <= register_address; -- Save target register address
+                        saved_mosi_data <= mosi_data; -- Save data to be written (valid in write op)
+                        saved_read_write <= read_write; -- Save read/write status bit
                     when others =>
                         state <= IDLE;
                 end case;
