1bbcb1ccd751d4e5745c302c47c9de9521be279f
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2023-10-02 21:04:21 -0300

    Completed start sequence generation

--- i2c_master/src/i2c_master.vhd
+++ i2c_master/src/i2c_master.vhd
@@ -38,12 +38,16 @@ architecture i2c_master_arq of i2c_master is
 
     constant IDLE: std_logic_vector(1 downto 0) := "00";
     constant START: std_logic_vector(1 downto 0) := "01";
+    constant WRITE_ADDR_W: std_logic_vector(1 downto 0) := "10";
     signal state: std_logic_vector(1 downto 0); -- FSM state
+    signal next_state: std_logic_vector(1 downto 0); -- To manage FSM state sequence
 
     signal saved_slave_address: std_logic_vector(ADDRESS_WIDTH downto 0);
     signal saved_register_address: std_logic_vector(REGISTER_WIDTH-1 downto 0);
     signal saved_mosi_data: std_logic_vector(DATA_WIDTH-1 downto 0);
     signal saved_read_write: std_logic;
+
+    signal process_counter: std_logic_vector(1 downto 0);
 
 begin
 
@@ -64,11 +68,33 @@ begin
                         serial_data <= '1'; -- Hold SDA high
                         serial_clock <= '1'; -- Hold SCL high
 
+                        process_counter <= "00"; -- No internal process
+
                         if enable = '1' then
                             state <= START; -- Proceed to START
+                            next_state <= WRITE_ADDR_W; -- Write address after START squence
                         else
                             state <= IDLE; -- Stay IDLE
+                            next_state <= IDLE;
                         end if;
+                    
+                    when START =>
+                        -- Generate START sequence
+                        case process_counter is
+                            when "00" =>
+                                process_counter <= "01";
+                            when "01" =>
+                                process_counter <= "10";
+                                serial_data <= '0'; -- SDA low
+                            when "10" =>
+                                process_counter <= "11";
+                            when "11" =>
+                                process_counter <= "00";
+                                serial_clock <= '0'; -- SCL low
+                                state <= next_state; -- Proceed to the next state
+                            when others =>
+                                state <= IDLE;
+                        end case;
 
                     when others =>
                         state <= IDLE;
